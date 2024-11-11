1a2463e2f175952aaa0faeeede5b1aef82667241
henrique <salleshenrique13@gmail.com>
2024-11-11 00:43:08 -0300

    Primeiro commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Conexao.Conexao;
+import org.example.Conexao.ConexaoMySQL;
+
+public class Main {
+    public static void main(String[] args) {
+        Conexao conn = new ConexaoMySQL();
+
+    }
+}
